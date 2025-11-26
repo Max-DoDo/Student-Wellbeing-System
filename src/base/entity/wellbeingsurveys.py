096bcb1aaf3aa44d366b6e1c54b2662981486eed
Max <great_maxwell@outlook.com>
2025-11-26 14:59:32 +0000

    modify: assessments file name. remove auto comment

--- src/base/entity/wellbeingsurveys.py
+++ src/base/entity/wellbeingsurveys.py
@@ -1,11 +1,4 @@
-'''
-Author: Max great_maxwell@outlook.com
-Date: 2025-11-26 14:46:26
-LastEditors: Max great_maxwell@outlook.com
-LastEditTime: 2025-11-26 14:57:36
-FilePath: \Student-Wellbeing-System\src\base\entity\wellbeingsurveys.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-'''
+
 from dataclasses import dataclass
 
 @dataclass
