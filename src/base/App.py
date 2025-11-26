f3a77cbd1e1b2952f4c30cf2994c45a699391652
Max <great_maxwell@outlook.com>
2025-11-26 01:14:57 +0000

    add: OOP structure tree added
    Added project structure tree

---
+++ src/base/App.py
@@ -0,0 +1,19 @@
+
+'''
+This class is the init class for the programme. Its UI object and database object are created in this class.
+This class should not be subclassed.
+'''
+
+class App:
+    
+    def __init__(self):
+        
+        self.main()
+
+    def main(self) -> None:
+        pass
+
+
+if __name__ == "__main__":
+    app = App()
+
