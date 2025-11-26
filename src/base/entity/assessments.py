b3140933729e820616ef22066875682c303c11f6
Max <great_maxwell@outlook.com>
2025-11-26 14:59:57 +0000

    del: remove auto comment

--- src/base/entity/assessments.py
+++ src/base/entity/assessments.py
@@ -1,11 +1,4 @@
-'''
-Author: Max great_maxwell@outlook.com
-Date: 2025-11-26 14:43:34
-LastEditors: Max great_maxwell@outlook.com
-LastEditTime: 2025-11-26 14:58:52
-FilePath: \Student-Wellbeing-System\src\base\entity\assessments.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-'''
+
 
 from dataclasses import dataclass
 
