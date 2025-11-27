abb485509b249ca337f46beae3decf49a4843583
Max <great_maxwell@outlook.com>
2025-11-27 22:07:43 +0000

    feature: implement assessment_repo class

--- src/base/entity/assessments.py
+++ src/base/entity/assessments.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from datetime import datetime
 
 @dataclass
 class Assessment:
@@ -7,4 +8,9 @@ class Assessment:
     assignment_name:str
     grade:int
     submitted_on_time:str
-    
+
+    def __post_init__(self):
+        super().__post_init__()
+        if self.submitted_on_time == None:
+            self.submitted_on_time = datetime.now().strftime("%Y-%m-%d")
+        
--- src/base/repository/assessment_repo.py
+++ src/base/repository/assessment_repo.py
@@ -1,4 +1,33 @@
-from base.repository.base_repo import Base_Repo
+from typing import List, Optional
+from entity.assessments import Assessment
+from repository.base_repo import Base_Repo
 
 class Assessment_Repo(Base_Repo):
-    pass;
+    
+    def getAssessment(self,id=int) -> Optional[Assessment]:
+        query = "SELECT * FROM assessments WHERE assessment_id = ?"
+        self.cursor.execute(query, (id,))
+        row = self.cursor.fetchone()
+        if row:
+            return self.toAssessment(row)
+        return None
+
+    def getAssessments(self) -> Optional[List[Assessment]]:
+        query = "SELECT * FROM assessments"
+        self.cursor.execute(query)
+        rows = self.cursor.fetchall()
+        if rows:
+            return self.toAssessments(rows)
+        return None
+
+    def toAssessment(self, row)-> Assessment:
+        return Assessment(
+            assessment_id=row["assessment_id"],
+            student_id=row["student_id"],
+            assignment_name=row["assignment_name"],
+            grade=row["grade"],
+            submitted_on_time=row["submitted_on_time"]
+        )
+
+    def toAssessments(self, rows) -> List[Assessment]:
+        return [self.toAssessment(row) for row in rows]
