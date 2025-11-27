2268d0c5934006ea29ed1384e5598073bb12d019
Max <great_maxwell@outlook.com>
2025-11-27 14:41:05 +0000

    Repo function structure and file rename to lowercases (#14)
    * add: all repo class structure
    
    I do not want write any discription
    
    * refactor: rename filename with placeholder
    
    * refactor: rename file name to lower cases

---
+++ src/base/repository/assessment_repo.py
@@ -0,0 +1,4 @@
+from repository.baserepo import BaseRepo
+
+class Assessment_Repo(BaseRepo):
+    pass;
---
+++ src/base/repository/student_repo.py
@@ -0,0 +1,9 @@
+from repository.baserepo import BaseRepo
+from base.entity.Student import Student
+class Student_Repo(BaseRepo):
+
+    def getStudentName(self, id) -> str:
+        pass;
+
+    def getAllStudent(self) -> Student:
+        pass;
