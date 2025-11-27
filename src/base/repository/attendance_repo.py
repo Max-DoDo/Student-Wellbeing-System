aaf922029db10ca67643705ac69d96b7b790c054
Max <great_maxwell@outlook.com>
2025-11-27 20:35:39 +0000

    refactor: rename wrong spelling for Attendance again

--- src/base/repository/attendance_repo.py
+++ src/base/repository/attendance_repo.py
@@ -5,10 +5,12 @@ from repository.base_repo import Base_Repo
 class Attendance_Repo(Base_Repo):
     
     
-    def getAttendence(self,id=int) -> Attendance:
-        query = "SELECT * FROM attendance WHERE users_id = ?"
+    def getAttendance(self,id=int) -> Attendance:
+        query = "SELECT * FROM attendance WHERE attendance_id = ?"
         self.cursor.execute(query, (id,))
         row = self.cursor.fetchone()
         if row:
             return self.toUser(row)
         return None
+    
+    def getAllAttendance(self):
